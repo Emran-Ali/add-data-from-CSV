cefb7d792eb2de0a1e109eca8ec17f9716e98665
Emran-Ali <emranitmbstu@gmail.com>
2024-02-19 12:22:41 +0600

    Refactor with OOP

---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<?php
+    include  './Action/Connection.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HTML 5 Boilerplate</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="div">
+        <a href="Action/Action.php">
+        <button class="btn " >Add Data</button>
+        </a>
+        <a href="Action/Truncagitte.php" >
+        <button class="btn"> Delete Data</button>
+        </a>
+
+    </div>
+
+</body>
+</html>
